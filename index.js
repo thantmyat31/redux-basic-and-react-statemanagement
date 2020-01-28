0711fd62ca4f52625341d98d05178d8875e276a3
thantmyat <thantmyat1234@gmail.com>
2020-01-28 11:10:24 +0630

    create redux basic sample

---
+++ index.js
@@ -0,0 +1,102 @@
+const redux = require('redux');
+const reduxLogger = require('redux-logger');
+
+const createStore = redux.createStore; //To store the initial state
+const combineReducer = redux.combineReducers; //To combine the muliple reducers
+const applyMiddleware = redux.applyMiddleware;
+const logger = reduxLogger.createLogger();
+
+//action start
+const BUY_CAKE = 'BUY_CAKE';
+const BUY_ICECREAM = 'BUY_ICECREAM';
+
+const buyCake = () => {
+	return {
+		type: BUY_CAKE,
+		info: 'action to buy cake'
+	};
+};
+const buyIceCream = () => {
+	return {
+		type: BUY_ICECREAM,
+		info: 'action to buy ice-cream'
+	};
+};
+
+//reducer start
+// const initialState = {
+// 	numberOfCake: 10,
+// 	numberOfIceCream: 20
+// };
+const initialCake = {
+	numberOfCake: 10
+};
+
+const initialIceCream = {
+	numberOfIceCream: 20
+};
+
+// const reducer = (state = initialState, action) => {
+// 	switch (action.type) {
+// 		case BUY_CAKE:
+// 			return {
+// 				...state,
+// 				numberOfCake: state.numberOfCake - 1
+// 			};
+
+// 		case BUY_ICECREAM:
+// 			return {
+// 				...state,
+// 				numberOfIceCream: state.numberOfIceCream - 1
+// 			};
+
+// 		default:
+// 			return state;
+// 	}
+// };
+
+const cakeReducer = (state = initialCake, action) => {
+	switch (action.type) {
+		case BUY_CAKE:
+			return {
+				...state,
+				numberOfCake: state.numberOfCake - 1
+			};
+
+		default:
+			return state;
+	}
+};
+
+const iceCreamReducer = (state = initialIceCream, action) => {
+	switch (action.type) {
+		case BUY_ICECREAM:
+			return {
+				...state,
+				numberOfIceCream: state.numberOfIceCream - 1
+			};
+
+		default:
+			return state;
+	}
+};
+
+//store start
+
+//create rootReducer to merge/combine the multiple reducers in one root
+const rootReducer = combineReducer({
+	cake: cakeReducer,
+	iceCream: iceCreamReducer
+});
+// store the application state
+const store = createStore(rootReducer, applyMiddleware(logger));
+//getState() --> this method access to get current state
+console.log('Initial state', store.getState());
+//subscribe() --> this method work as returned listener when state is updated
+const unsubscribe = store.subscribe(() => {});
+//dispatch(action) --> this method access the action as parameter to update the state
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+store.dispatch(buyIceCream());
+unsubscribe();
